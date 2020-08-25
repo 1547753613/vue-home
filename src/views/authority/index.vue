791021a72c2724afcf1f20d66ffb7b67f6b923af
chentianhao <1547753613@qq.com>
2020-08-25 17:37:13 +0800

    陈天昊第二次修改

--- src/views/authority/index.vue
+++ src/views/authority/index.vue
@@ -1,32 +1,91 @@
 <template>
     <div>
       <h1>权限管理</h1>
-      <el-collapse>
-        <el-collapse-item  v-for="(item,index) in depts" :title="item.dname" :name="item.did">
-          <div><el-button size="mini">测试人员</el-button><el-button size="mini">测试经理</el-button></div>
+      <el-collapse accordion @change="Collchange" v-model="did">
+        <el-collapse-item  v-for="(i,index) in depts" :title="i.dname" :name="i.did">
+          <div>
+            <el-button v-for="item in role" size="mini" @click="SelectRole(item.rid)">{{ item.rname}}</el-button>
+          </div>
+
+
         </el-collapse-item>
 
       </el-collapse>
+      <el-button :disabled="bool" size="medium" @click="setRoleAuth">修改权限</el-button>
+      <el-tree
+        :data="data"
+        show-checkbox
+        node-key="id"
+        :default-expanded-keys="[1]"
+        :default-checked-keys="checked"
+        ref="tree">
+      </el-tree>
     </div>
 </template>
 
 <script>
-  import {get} from "../../request/Http";
+  import {get,put1} from "../../request/Http";
+  import {inform} from "../../utils/inform";
 
   export default {
         name: "index",
       data(){
           return{
-            depts:[
-              {did:1001,dname:'测试部'},
-              {did:1002,dname:'人事部'},
-              {did:1003,dname:'客服部'}
-            ]
+            rid:0,
+            bool:true,
+            depts:[],
+            data:[],
+            role:[],
+            checked:[],
+            did:1
           }
       },
+    methods:{
+      setRoleAuth(){
+        let obj=[...this.$refs.tree.getCheckedKeys(),...this.$refs.tree.getHalfCheckedKeys()];
+        put1('/authority/addAuth',{ints:obj,rid:this.rid}).then(data=>{
+          if(data.code==200){
+            inform(data,()=>{
+              this.$refs.tree.setCheckedKeys([]);
+              this.bool=true;
+            })
+          }
+        })
+      },
+      setCheckedKeys() {
+        this.$refs.tree.setCheckedKeys([3]);
+      },
+      Collchange(did){
+        this.bool=true;
+        if (null!=did) {
+          get('/Department/queryrole', {did}).then(data => {
+            this.role=data;
+
+           // console.log(data)
+          })
+        }
+      },
+      SelectRole(rid){
+        this.rid=rid;
+        this.bool=false;
+        this.$refs.tree.setCheckedKeys([]);
+        get('/authority/queryrid',{rid}).then(data=>{
+          this.checked=data;
+          this.checked = this.checked.filter(item =>{
+              let arr=[1,2,3,4,5,6,13,]
+            return arr.indexOf(item)==-1;
+          });
+
+        })
+       // console.log(rid)
+      }
+    },
       created(){
         get('/authority/queryall').then(data=>{
-          console.log(data)
+          this.data=data;
+        })
+        get('/Department/query').then(data=>{
+          this.depts=data
         })
       }
     }
--- src/views/person/index.vue
+++ src/views/person/index.vue
@@ -1,15 +1,174 @@
 <template>
     <div>
-      <h1>员工管理</h1>
+     <el-table
+
+       :row-class-name="tableRowClassName"
+        :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
+        style="width: 100%;">
+       <el-table-column
+         label="编号"
+         prop="id"
+         width="50">
+       </el-table-column>
+       <el-table-column
+         label="账号"
+         prop="aname"
+         width="100">
+       </el-table-column>
+       <el-table-column
+         label="真实姓名"
+         prop="name"
+         width="100">
+       </el-table-column>
+       <el-table-column
+         label="性别"
+         :formatter="format"
+
+         prop="gender"
+         width="100">
+       </el-table-column>
+       <el-table-column
+         label="手机号"
+         prop="phone"
+         width="150">
+       </el-table-column>
+
+       <el-table-column
+         label="籍贯"
+         prop="nativePlave"
+         width="100">
+       </el-table-column>
+       <el-table-column
+         label="入职日期"
+         prop="beginDate"
+         width="100">
+       </el-table-column>
+       <el-table-column
+         label="离职日期"
+         prop="endDate"
+         :formatter="format"
+         width="190">
+       </el-table-column>
+       <el-table-column
+         label="职位"
+         prop="role.rname"
+         width="100">
+       </el-table-column>
+       <el-table-column
+         label="状态"
+         prop="isenble"
+         :formatter="format"
+         width="100">
+       </el-table-column>
+        <el-table-column
+          align="right">
+          <template slot="header" slot-scope="scope">
+            <el-input
+              v-model="search"
+              size="mini"
+              placeholder="输入关键字搜索"/>
+          </template>
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row,'修改')"
+            >修改</el-button>
+            <el-button v-if="scope.row.isenble==1"
+                       size="mini"
+                       type="danger">
+              锁定
+            </el-button>
+            <el-button v-else
+                        size="mini"
+                        type="primary">
+              解锁
+            </el-button>
+           <!-- <el-button
+              size="mini"
+              type="danger"
+              @click="handleEdit(scope.$index, scope.row,'删除')"
+            >{{ scope.row.isenble==1?'锁定':'解封'}}</el-button>-->
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-row>
+
+        <el-col :span="3" :offset="5">
+          <el-pagination
+            background
+            layout="sizes,total, prev, pager, next, jumper"
+            :total="total"
+            :page-sizes="[3, 5, 10, 20]"
+            :page-size="pageSize"
+            @current-change="handleCurrentChange"
+            @size-change="handleSizeChange"
+          >
+          </el-pagination>
+        </el-col>
+      </el-row>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "index"
+
+  import {get} from "../../request/Http";
+
+  function pageAdmin(pageNum,pageSize,th_is) {
+    get('/person/select',{pageNum,pageSize}).then(data=>{
+      th_is.tableData=data.list;
+      th_is.total=data.total;
+      th_is.pageSize=data.pageSize
+    })
+  }
+
+  export default {
+        name: "index",
+        data(){
+          return{
+            tableData:[],
+            search: '',
+            total:0,
+            pageSize:0,
+            val:3
+          }
+        },
+      created(){
+        pageAdmin(null,null,this);
+      },
+    methods:{
+      handleEdit(){},
+      handleSizeChange(val){
+        this.val=val;
+        pageAdmin(this.pageNum,val,this)
+
+      },
+      handleCurrentChange(pageNum){
+        this.pageSize=pageNum;
+        pageAdmin(pageNum,this.val,this)
+      },
+      format: function(row, column) {
+        if (column.property=="gender"){
+          return row.gender==1?'男':'女'
+        }else if (column.property=='endDate') {
+          return row.endDate==null?'在职':row.endDate
+        }else if (column.property=='isenble'){
+          return row.isenble==1?'正常':'锁定'
+        }
+      },
+      tableRowClassName(row,rowIndex) {
+        if(row.row.isenble===0){
+          return 'row';
+        }
+        return '';
+
+      }
+      }
     }
 </script>
 
-<style scoped>
+<style>
+  .el-table .row{
+      background: #99FF99
 
+    }
 </style>
